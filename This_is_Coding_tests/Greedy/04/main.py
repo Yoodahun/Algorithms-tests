64df3b9235ff23b50cd39655f048c1ef5a11d149
Yoodahun <dahun4032@gmail.com>
2021-08-24 00:37:00 +0900

    UPDATE Greedy - 1이 될 떄까지

--- This_is_Coding_tests/Greedy/04/main.py
+++ This_is_Coding_tests/Greedy/04/main.py
@@ -2,11 +2,10 @@ n, k = map(int, input().split())
 
 result = 0
 
-
 while True:
-    #N이 K로 나누어 떨어지는 수가 될 때까지만 1씩 빼기
-    target = (n//k) * k
-    result += (n-target)
+    # N이 K로 나누어 떨어지는 수가 될 때까지만 1씩 빼기
+    target = (n // k) * k
+    result += (n - target)
     n = target
     # N이 K보다 작을때 반복문 탈출
 
@@ -15,6 +14,5 @@ while True:
     result += 1
     n //= k
 
-result += (n-1)
+result += (n - 1)
 print(result)
-
